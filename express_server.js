1f51be768fc0481a7450446860f39dc92afa1c7d
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-09-03 01:00:53 -0400

    implemented cookie parser

--- express_server.js
+++ express_server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+var cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
