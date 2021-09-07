4ea2003fe3ea947c8f3f4ae1d1354e9deb3e4efa
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-09-06 21:31:22 -0400

    added cookie to handle username

--- express_server.js
+++ express_server.js
@@ -79,6 +79,11 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect(`/urls/${req.params.shortURL}`);
 });
 
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
