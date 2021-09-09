3bbb8833c0ef3bd8aa50d25716104aa2b2a3fbff
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-09-09 00:14:05 -0400

    moved random string to helpers

--- express_server.js
+++ express_server.js
@@ -13,18 +13,11 @@ app.use(cookieSession({
 
 const bcrypt = require('bcrypt');
 
-const { getUserByEmail } = require('./helpers.js');
+const { getUserByEmail, generateRandomString } = require('./helpers.js');
 
 const urlDatabase = {};
 
 const users = {};
-
-const generateRandomString = () => {
-  let out = '';
-  let base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 6; i++) out += base.charAt(Math.floor(Math.random()*base.length))
-  return out;
-}
 
 app.get("/urls", (req, res) => {
   let urlUserDatabase = {};
--- helpers.js
+++ helpers.js
@@ -7,4 +7,11 @@ const getUserByEmail = (email, database) => {
   return undefined;
 }
 
-module.exports = { getUserByEmail };
+const generateRandomString = () => {
+  let out = '';
+  let base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) out += base.charAt(Math.floor(Math.random()*base.length))
+  return out;
+}
+
+module.exports = { getUserByEmail, generateRandomString };
